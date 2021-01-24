86f302002ce32d32783fff248f23420c72a7d378
Ryan <ryansblachowicz@gmail.com>
2021-01-24 10:50:34 -0500

    scale_gen works

--- proc_gen.py
+++ proc_gen.py
@@ -15,26 +15,31 @@ def scale_gen(key, mode):
     # Modes
     major = "WWHWWWH"
     minor = "WHWWHWW"
+    if mode == "major":
+        mode = major
+    elif mode == "minor":
+        mode = minor
 
     # Build Scale
     pos = notes.index(key) # Declare as root
     scale = []
-    for i in range(len(mode) + 1):
+    for i in range(len(mode)):
         if i == 0:
             scale.append(notes[pos]) 
-        elif key[i - 1]  == "H":
+        elif mode[i - 1]  == "H":
             pos = pos + 1
             # Wrap
-            if notes[pos] > len(notes):
-                pos = pos - len(notes)
+            if pos == len(notes):
+                pos = pos - len(notes)  
             scale.append(notes[pos]) 
-        elif key[i - 1] == "W":
+        elif mode[i - 1] == "W":
             pos = pos + 2
             # Wrap
-            if notes[pos] > len(notes):
+            if pos == len(notes): # len doesnt start 0 so == is going over
                 pos = pos - len(notes)
-            elif notes[pos] == len(notes) + 1:
+            elif pos == len(notes) - 1:
                 pos = pos - len(notes)
             scale.append(notes[pos]) 
+    print(scale)
 
-scale_gen("c", "major")
+scale_gen("g", "major")
